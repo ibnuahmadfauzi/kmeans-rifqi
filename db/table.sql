72fbf925a1cd58bfa2f51a4e63254a6947ab711b
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-07-06 19:04:00 +0700

    add table

--- db/table.sql
+++ db/table.sql
@@ -7,3 +7,9 @@ CREATE TABLE usulan (
     biaya INT
 );
 
+INSERT INTO usulan (jenis_usulan, kondisi, manfaat, waktu, biaya) VALUES
+('Aspal burda Jl. Masjid - Pondok', 3, 3, 40, 93234000),
+('Aspal burda Jl. Pondok RW. 02', 3, 2, 60, 150639000),
+('Aspal burda Jl. Sido rukun RW. 01', 3, 3, 40, 75504000),
+('Penambalan Aspal burda Dsn. Bulu RW. 05', 2, 3, 20, 80128000),
+('Aspal burda Dsn. Bulu RW. 08', 3, 2, 18, 17465000);
